7ceeb36de3c799d6ea111346ae784c0b49f2252f
WangMC <2843372310@qq.com>
2024-10-18 23:39:23 +0800

    test

--- Tree/main.cpp
+++ Tree/main.cpp
@@ -4,5 +4,7 @@ int main()
 	BinaryTree bin_tree{ 1,5,82,83,80,4,6,8,3,48,65,879,546,784,8654,8749,617,874,627,489 ,75};
 	bin_tree.print();
 	cout << bin_tree.treeHeight();
+	bin_tree.remove(82);
+	bin_tree.print();
 	return 0;
 }
